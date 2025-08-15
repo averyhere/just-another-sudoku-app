b30e5d5a955f30f3d69f36049dc69366e026f2ca
Avery Ondo <hello@averyhere.com>
2025-08-15 10:35:10 -0600

    remove pkg

--- src/app/(tabs)/gamecenter.tsx
+++ src/app/(tabs)/gamecenter.tsx
@@ -1,16 +1,7 @@
 import { View, StyleSheet } from "react-native"
 import { Text } from "@/components/Text"
-import { useGameCenter } from "expo-game-center"
-
-import config from "@/config"
 
 export default function Tab() {
-  const { status } = useGameCenter({
-    leaderboards: {
-      bestScore: config.gameCenterLeaderboardId,
-    },
-  })
-
   console.log("Game Center status:", status)
   return (
     <View style={styles.container}>
